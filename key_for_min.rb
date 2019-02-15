40d39fab57b355ba8d59d59f3cf9650de32c344f
lieby weintraub <liebahinda@gmail.com>
2019-02-15 04:35:53 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,15 +3,5 @@
 
 def key_for_min_value(name_hash)
   if name_hash.empty?
-    return nil 
-else name_hash1 = name_hash.sort_by{|k, v| v} 
-<<<<<<< HEAD
-  hash2=name_hash1[0]
- hash2 [0]
+    return nil
   
-end   
-=======
-  
-  
->>>>>>> 5e0134e7132f26dad2cd0b5acf8cf71d7a36f9f9
-end
